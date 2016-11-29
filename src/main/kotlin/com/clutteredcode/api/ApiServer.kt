05cce0461ede665a0b1714fdc9b95ad9c637feed
David Clutter <cluttered.code@gmail.com>
2016-11-29 00:47:57 -0700

    MVP

--- src/main/kotlin/com/clutteredcode/api/ApiServer.kt
+++ src/main/kotlin/com/clutteredcode/api/ApiServer.kt
@@ -1,5 +1,11 @@
 package com.clutteredcode.api
 
+import spark.Spark.get
+
 /**
  * Created by david on 11/28/16.
  */
+
+fun main(args : Array<String>) {
+    get("/ping", { request, response -> "pong" })
+}
